ed183072f0edbbf5c7eafbc336bc0c368b684221
shashikanth <mallepally.shashikanthh@yahoo.com>
2023-02-15 17:12:02 +0530

    Create 2549. Count Distinct Numbers on Board.java

---
+++ 2549. Count Distinct Numbers on Board.java
@@ -0,0 +1,8 @@
+//problem: https://leetcode.com/problems/count-distinct-numbers-on-board/
+class Solution {
+    public int distinctIntegers(int n) {
+        if (n==1)
+        return 1;
+        return n-1;
+    }
+}
