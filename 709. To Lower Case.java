e462a3dc7b0541cdf9eb752ee6f958698664ccb2
shashikanth <mallepally.shashikanthh@yahoo.com>
2023-02-21 13:12:44 +0530

    Create 709. To Lower Case.java

---
+++ 709. To Lower Case.java
@@ -0,0 +1,7 @@
+709. To Lower Case.java
+problem : https://leetcode.com/problems/to-lower-case/
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+} 
