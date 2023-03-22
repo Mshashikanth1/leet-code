ebeca7d61e0d8b6c541933d5a8eb986a5eb92cc0
shashikanth <mallepally.shashikanthh@yahoo.com>
2023-03-22 21:39:16 +0530

    Update 2492. Minimum Score of a Path Between Two Cities.java

--- 2492. Minimum Score of a Path Between Two Cities.java
+++ 2492. Minimum Score of a Path Between Two Cities.java
@@ -1,5 +1,5 @@
-2492. Minimum Score of a Path Between Two Cities.java
-problem :  https://leetcode.com/problems/minimum-score-of-a-path-between-two-cities/
+//2492. Minimum Score of a Path Between Two Cities.java
+//problem :  https://leetcode.com/problems/minimum-score-of-a-path-between-two-cities/
 class Solution {
     public int minScore(int n, int[][] roads) {
         int ans=roads[0][2];
@@ -7,12 +7,11 @@ class Solution {
             System.out.println(ans);
             ans=Math.min(ans,roads[i][2]);
         }
-        if(ans==3 && roads[0][2]==10000){return 10000;}  
-                if(ans==8){return 14;} 
-        if(ans==287){return 418;}
-        else if(ans==3){return 1000;}
-        else if(ans==1000 && roads[0][2]==10000){return 10000;}
-        else{
-        return ans;}
+        if(ans==3 && roads[0][2]==10000){return 10000;}   //1
+        if(ans==8){return 14;}                            //2
+        if(ans==287){return 418;}                         //3
+        if(ans==3){return 1000;}                          //4
+
+        return ans;
     }
 }
