fdd602926df77ba830c58536f0ff221391d6918b
shashikanth <mallepally.shashikanthh@yahoo.com>
2023-04-11 11:27:15 +0530

    Create 1920. Build Array from Permutation.java

---
+++ 1920. Build Array from Permutation.java
@@ -0,0 +1,14 @@
+//1920. Build Array from Permutation.java
+//problem: https://leetcode.com/problems/build-array-from-permutation/
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n=nums.length;
+        int[] ans=new int[n];
+        for(int i=0;i<n;i++){
+            ans[i]=nums[nums[i]];
+        }
+
+        return ans;
+        
+    }
+}
