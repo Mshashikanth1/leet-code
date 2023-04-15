a16a4e9cad47ee938f26d571d13784fb81a69125
shashikanth <mallepally.shashikanthh@yahoo.com>
2023-04-15 12:11:22 +0530

    Create 2218. Maximum Value of K Coins From Piles.java

---
+++ 2218. Maximum Value of K Coins From Piles.java
@@ -0,0 +1,29 @@
+/*
+2218. Maximum Value of K Coins From Piles.java
+Problem : https://leetcode.com/problems/maximum-value-of-k-coins-from-piles/description/
+ */
+
+class Solution {
+    public int maxValueOfCoins(List<List<Integer>> piles, int k) {
+        
+        int n=piles.size();
+
+        int[][] dp=new int[n+1][k+1];
+
+        for(int i=1;i<=n;i++){
+
+            for(int coins=0;coins<=k;coins++){
+
+                int currentSum=0;
+                for( int currentCoins=0; currentCoins<=Math.min((int)piles.get(i-1).size(),coins); currentCoins++ ){
+                    if(currentCoins>0){
+                        currentSum+=piles.get(i-1).get(currentCoins-1);
+                    }
+                    dp[i][coins]=Math.max(dp[i][coins],dp[i-1][coins-currentCoins]+currentSum);
+                }
+            }
+
+        }
+        return dp[n][k];
+    }
+}
