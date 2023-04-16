92d7cb66931863922521f59a4c1c60b33db8735a
shashikanth <mallepally.shashikanthh@yahoo.com>
2023-04-16 08:13:24 +0530

    Create 6376. Row With Maximum Ones.java

---
+++ 6376. Row With Maximum Ones.java
@@ -0,0 +1,73 @@
+/*
+6376. Row With Maximum Ones
+
+    User Accepted: 3845
+    User Tried: 4062
+    Total Accepted: 3853
+    Total Submissions: 4560
+    Difficulty: Easy
+
+Given a m x n binary matrix mat, find the 0-indexed position of the row that contains the maximum count of ones, and the number of ones in that row.
+
+In case there are multiple rows that have the maximum count of ones, the row with the smallest row number should be selected.
+
+Return an array containing the index of the row, and the number of ones in it.
+
+ 
+
+Example 1:
+
+Input: mat = [[0,1],[1,0]]
+Output: [0,1]
+Explanation: Both rows have the same number of 1's. So we return the index of the smaller row, 0, and the maximum count of ones (1). So, the answer is [0,1]. 
+
+Example 2:
+
+Input: mat = [[0,0,0],[0,1,1]]
+Output: [1,2]
+Explanation: The row indexed 1 has the maximum count of ones (2). So we return its index, 1, and the count. So, the answer is [1,2].
+
+Example 3:
+
+Input: mat = [[0,0],[1,1],[0,0]]
+Output: [1,2]
+Explanation: The row indexed 1 has the maximum count of ones (2). So the answer is [1,2].
+
+ 
+
+Constraints:
+
+    m == mat.length 
+    n == mat[i].length 
+    1 <= m, n <= 100 
+    mat[i][j] is either 0 or 1.
+
+
+
+
+*/
+class Solution {
+    public int[] rowAndMaximumOnes(int[][] mat) {
+    int maxOnes = 0;
+        int maxRow = 0;
+        for (int i = 0; i < mat.length; i++) {
+            int ones = countOnes(mat[i]);
+            if (ones > maxOnes) {
+                maxOnes = ones;
+                maxRow = i;
+            }
+        }
+        return new int[]{maxRow, maxOnes};
+    }
+
+    private int countOnes(int[] row) {
+        int count = 0;
+        for (int i = 0; i < row.length; i++) {
+            if (row[i] == 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
