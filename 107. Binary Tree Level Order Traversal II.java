79bc56f76f8d212d2348c2e03a7529a992c639bf
shashikanth <mallepally.shashikanthh@yahoo.com>
2023-07-25 22:12:24 +0530

    Create 107. Binary Tree Level Order Traversal II.java

---
+++ 107. Binary Tree Level Order Traversal II.java
@@ -0,0 +1,77 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<Integer,List<Integer>> adjList;
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+        adjList=new HashMap<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        dfs(root,0);
+        for(int i: adjList.keySet()){
+            ans.add(adjList.get(i));
+        }
+
+        Collections.reverse(ans);
+        return ans;
+    }
+    void dfs(TreeNode root,int depth){
+        if(root==null) return;
+        List<Integer> lis;
+        if(adjList.containsKey(depth)) lis=adjList.get(depth);
+        else lis=new ArrayList();
+        lis.add(root.val);
+
+        adjList.put(depth,lis);
+        dfs(root.left,depth+1);
+        dfs(root.right,depth+1);
+    }
+}
+
+/*
+107. Binary Tree Level Order Traversal II
+Medium
+4.5K
+312
+Companies
+
+Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
+
+ 
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[15,7],[9,20],[3]]
+
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+
+Example 3:
+
+Input: root = []
+Output: []
+
+ 
+
+Constraints:
+
+    The number of nodes in the tree is in the range [0, 2000].
+    -1000 <= Node.val <= 1000
+
+
+ */
