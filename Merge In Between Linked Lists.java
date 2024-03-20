a832528c97dcead7aed072b26d866fb458e7a2b4
Mallepally shashi kanth varma <mallepally.shashikanthh@yahoo.com>
2024-03-20 11:26:58 +0530

    Create Merge In Between Linked Lists.java

---
+++ Merge In Between Linked Lists.java
@@ -0,0 +1,82 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+ /**
+   Approach : identify the ath and bth node by tracking the count and adject the ath node next to be
+   head of the sec list, and adjust by not loosing the linked list one and contiue seaching in the linked list 
+   to find the bth node once found stop iterating over the linked list and set the tail of linkedlist 2 to next of 
+   bth node 
+
+   Time : O(n+m)
+   Space :O(1)
+  */
+class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+            ListNode curr=list1, tail=list2;
+            while(tail.next!=null) tail=tail.next;
+            int c=1;
+            while(curr.next!=null){
+                if(c==a) {
+                    ListNode next=curr.next.next;
+                    curr.next=list2;
+                    curr=next;
+                }
+                if(c==b){
+                    break;
+                }
+                curr=curr.next;
+                c++;
+            }
+            tail.next=curr;
+            return list1;
+    }
+}
+
+/**
+1669. Merge In Between Linked Lists
+Solved
+Medium
+Topics
+Companies
+Hint
+You are given two linked lists: list1 and list2 of sizes n and m respectively.
+
+Remove list1's nodes from the ath node to the bth node, and put list2 in their place.
+
+The blue edges and nodes in the following figure indicate the result:
+
+
+Build the result list and return its head.
+
+ 
+
+Example 1:
+
+
+Input: list1 = [10,1,13,6,9,5], a = 3, b = 4, list2 = [1000000,1000001,1000002]
+Output: [10,1,13,1000000,1000001,1000002,5]
+Explanation: We remove the nodes 3 and 4 and put the entire list2 in their place. The blue edges and nodes in the above figure indicate the result.
+Example 2:
+
+
+Input: list1 = [0,1,2,3,4,5,6], a = 2, b = 5, list2 = [1000000,1000001,1000002,1000003,1000004]
+Output: [0,1,1000000,1000001,1000002,1000003,1000004,6]
+Explanation: The blue edges and nodes in the above figure indicate the result.
+ 
+
+Constraints:
+
+3 <= list1.length <= 104
+1 <= a <= b < list1.length - 1
+1 <= list2.length <= 104
+
+
+ */
