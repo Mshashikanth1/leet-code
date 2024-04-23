c9ba85f0a2bab55e9a575d1f23715cf80f5fc34d
Mallepally shashi kanth varma <mallepally.shashikanthh@yahoo.com>
2024-04-23 17:05:33 +0530

    Create reverses_recursion.java

---
+++ reverses_recursion.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void reverseString(char[] s) {
+        swap(s,0,s.length-1);
+    }
+    
+    public void swap(char[] s, int i, int j){
+        if(i<j){
+            
+            char temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            
+            swap(s, i+1, j-1);
+        }
+    }
+}
+
+/*
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is a printable ascii character.
+*/
