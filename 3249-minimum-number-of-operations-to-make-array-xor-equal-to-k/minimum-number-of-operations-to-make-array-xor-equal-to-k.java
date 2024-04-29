1890882804968785afeffd3a60fcabdd5b3025f2
Mallepally shashi kanth varma <mallepally.shashikanthh@yahoo.com>
2024-04-29 12:43:05 +0530

    Time: 1 ms (100.00%) | Memory: 61.5 MB (5.18%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.java
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.java
@@ -1,16 +1,25 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        int xor=0;
-        for(int i:nums) xor^=i;
+        int res=0;
+        for(int i:nums) res^=i;
 
-        String b1=String.format("%32s", Integer.toBinaryString(xor)).replace(' ', '0');
-        String b2=String.format("%32s", Integer.toBinaryString(k)).replace(' ', '0');
+        int[] b1= toBinaryRepArr(res), b2= toBinaryRepArr(k);
+
 
         int c=0;
-        for(int i=0;i<32;i++){
-            if(b1.charAt(i)!=b2.charAt(i)) c++;
+        for(int i=0; i<32; i++){
+            if(b1[i]!=b2[i]) c++;
         }
 
         return c;
+    }
+
+    public int[] toBinaryRepArr(int n){
+        int[] bin =new int[32]; int i=31;
+        while(n>0){
+            bin[i--]=n%2;
+            n/=2;
+        }
+        return bin;
     }
 }
