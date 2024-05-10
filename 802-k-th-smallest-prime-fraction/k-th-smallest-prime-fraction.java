584de22fe92b1c58b3b6ef4bec2b49f694dd091d
Mallepally shashi kanth varma <mallepally.shashikanthh@yahoo.com>
2024-05-10 11:19:49 +0530

    Time: 442 ms (45.41%) | Memory: 126.1 MB (6.11%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
@@ -1,14 +1,23 @@
 class Solution {
-    public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        List<int[]> fractions= new  ArrayList<>();
 
+    /** Time :O(n*n) Space : O(n) */
+    public int[] kthSmallestPrimeFraction(int[] arr, int K) {
+        int[][] fractions= new int[arr.length*arr.length][2];
+
+        int k=0;
         for(int i=0; i<arr.length-1; i++){
             for(int j=arr.length-1; j>i; j--){
-                fractions.add(new int[]{ arr[i], arr[j] });
+                fractions[k++] =(new int[]{ arr[i], arr[j] });
             }
         }
-        
-        Collections.sort(fractions, (a, b) -> (a[0] * b[1] - a[1] * b[0]));
-        return fractions.get(k-1);
+
+        Arrays.sort(fractions, (a, b) -> (a[0] * b[1] - a[1] * b[0]));
+        return fractions[K-1];
+    }
+
+    public int factorial(int n){
+        int fact=1;
+        for(int i=1; i<=n; i++) fact*=i;
+        return fact;
     }
 }
